68cd72beb1c9e180c9969cfb287361f8d060d298
hariharen9 <hurryharen@gmail.com>
2025-09-01 13:34:59 +0530

    fix(build): add template data and resources for packaging and installation
    - Include enhance_this/templates in PyInstaller build with OS-dependent separator
    - Add MANIFEST.in to include template files in package distribution
    - Add Python package resources to Homebrew formula for dependencies
    - Enable setuptools to include package data automatically in pyproject.toml
    - Configure package discovery for enhance_this modules in pyproject.toml

--- Formula/enhance-this.rb
+++ Formula/enhance-this.rb
@@ -11,6 +11,36 @@ class EnhanceThis < Formula
 
   depends_on "python@3.11"
 
+  resource "click" do
+    url "https://pypi.io/packages/source/c/click/click-8.1.7.tar.gz"
+    sha256 "ca9853ad459e78b31683208753b81ece57b6b4863e0330d93a8a47e08b485497"
+  end
+
+  resource "requests" do
+    url "https://pypi.io/packages/source/r/requests/requests-2.31.0.tar.gz"
+    sha256 "942c3a62805036524852309990243c27a03575b2342c06f735638df83343ed91"
+  end
+
+  resource "pyyaml" do
+    url "https://pypi.io/packages/source/P/PyYAML/PyYAML-6.0.1.tar.gz"
+    sha256 "565d219cb938e6c18298551b4a4b39709b3f0a54218e4a855a45dc1b8a2c8a5b"
+  end
+
+  resource "pyperclip" do
+    url "https://pypi.io/packages/source/p/pyperclip/pyperclip-1.8.2.tar.gz"
+    sha256 "b9c60536f60b2a157b4f67c3c15c11c053f745344c365534403c95a89c0013c7"
+  end
+
+  resource "rich" do
+    url "https://pypi.io/packages/source/r/rich/rich-13.7.1.tar.gz"
+    sha256 "a2d38e15f2a3648427a0f423e0651412a6b797153a5471f03f3139a625591ce7"
+  end
+
+  resource "questionary" do
+    url "https://pypi.io/packages/source/q/questionary/questionary-2.0.1.tar.gz"
+    sha256 "313461388279236405d834b9c3699018a4646435415c668b8a3a663c45148668"
+  end
+
   def install
     virtualenv_install_with_resources
   end
